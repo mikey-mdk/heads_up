db697a4b9a67b83332b0c46576f61ac22e48aa13
mikey-mdk <michael.david.mdk@gmail.com>
2025-04-09 16:25:57 -0600

    Adding tips workflow

---
+++ lib/heads_up_web/controllers/tips_controller.ex
@@ -0,0 +1,11 @@
+defmodule HeadsUpWeb.TipsController do
+  use HeadsUpWeb, :controller
+
+  alias HeadsUp.Tips
+
+  def index(conn, _params) do
+    tips = Tips.list_tips()
+
+    render(conn, :index, tips: tips)
+  end
+end
